a94e8d4f599cc404f75aca976db0f2e7809d8dba
souvik_1234 <roysouvikr112@gmail.com>
2024-09-28 00:04:10 +0530

    dynamic post API

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "dev",
-        lastName: "Roy",
-        password: "dev@123",
-    });
+    console.log(req.body);
+    const user = new User(req.body);
 
     try {
         await user.save();
