80eec727413d89c1a85a2cc2b8e14903489199e6
souvik_1234 <roysouvikr112@gmail.com>
2024-09-29 13:14:55 +0530

    schema

--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,50 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
+        trim: true
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true
     },
     password: {
         type: String,
+        required: true
     },
     age: {
         type: Number,
+        min: 18,
     },
     gender: {
         type: String,
+        validate(value){
+            if(!["male", "female", "others"].includes(value)){
+                throw new Error("Gender data is not valid")
+            }
+        }
     },
+    photoUrl: {
+        type: String,
+        default: "https://www.vecteezy.com/free-vector/default-profile-picture"
+    },
+    about: {
+        type: String,
+        default: "This is a default about of the user"
+    },
+    skills: {
+        type: [String]
+    }
+},{
+    timestamps: true,
 });
 
 
