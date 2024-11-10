2a00c443e52237abf2d75e31b7e3fe3ea06d22f1
Souvik-R <roysouvikr112@gmail.com>
2024-11-11 00:38:29 +0530

    dynamic msg changes

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -2,12 +2,14 @@ const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
     try {
         const fromUserId = req.user._id;
         const toUserId = req.params.toUserId;
         const status = req.params.status;
+        const fromUserName = req.user.firstName;
 
         const allowedStatus = ["ignored", "interested"];
         if(!allowedStatus.includes(status)){
@@ -25,6 +27,11 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
             return res.status(400).json({message: "Connection request already exist"});
         };
 
+        const toUser = await User.findById(toUserId);
+        if (!toUser) {
+            return res.status(404).json({ message: "Recipient user not found" });
+        }
+
         const connectionRequest = new ConnectionRequest({
             fromUserId,
             toUserId,
@@ -34,7 +41,9 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
         const data = await connectionRequest.save();
 
         res.json({
-            message: "Connection Request Sent Successfully",
+            message: status === "interested"
+            ? `${fromUserName} has shown interest in connecting with ${toUser.firstName}.`
+            : `${fromUserName} has ignored ${toUser.firstName}.`,
             data
         });
 
